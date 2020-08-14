939b3ef84fe4e84ecd71ac2ec2ea2ccc2d54aab1
aymanMahgoub <ayman.mahgoub1@gmail.com>
2020-08-14 20:53:49 +0200

    [add] function to handle draw first or last line

--- Src/App/AbstractShape.php
+++ Src/App/AbstractShape.php
@@ -12,6 +12,20 @@ Abstract class AbstractShape
     /**
      * @param int $height
      */
-    public abstract function DrawShape(int $height);
+    public abstract function drawShape(int $height);
+
+    /**
+     * @param int $height
+     */
+    protected function drawFirstOrLastLine(int $height)
+    {
+        for ($theBeginningOfLine = 0; $theBeginningOfLine <= $height; $theBeginningOfLine++) {
+            if ($theBeginningOfLine == $height) {
+                echo "+";
+            } else {
+                echo " ";
+            }
+        }
+    }
 
 }
