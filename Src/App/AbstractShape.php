d3da73754f1b22b2d47d755f501a536750947146
aymanMahgoub <ayman.mahgoub1@gmail.com>
2020-08-14 20:48:22 +0200

    [add] AbstractShape and create DrawShape abstractFunction

---
+++ Src/App/AbstractShape.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Src\App;
+
+/**
+ * Class AbstractShape
+ *
+ * @package Src\App
+ */
+Abstract class AbstractShape
+{
+    /**
+     * @param int $height
+     */
+    public abstract function DrawShape(int $height);
+
+}
