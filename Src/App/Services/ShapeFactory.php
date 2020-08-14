63537fcced2552bbc741feb5b9b37daff66dd0ef
aymanMahgoub <ayman.mahgoub1@gmail.com>
2020-08-14 21:35:04 +0200

    [create] ShapeFactory service and hancle get shape by type

---
+++ Src/App/Services/ShapeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Services;
+
+use Constant\ShapeConstant;
+use InvalidArgumentException;
+
+/**
+ * Class ShapeFactory
+ *
+ * @package Services
+ */
+class ShapeFactory
+{
+    /**
+     * @param string $type
+     *
+     * @return AbstractShape
+     */
+    public function getShapeByType(string $type): AbstractShape
+    {
+        if ($type === ShapeConstant::TREE) {
+            return new TreeShape();
+        } elseif ($type === ShapeConstant::STAR) {
+            return new starShape();
+        }
+        throw new InvalidArgumentException('Passed invalid shape type');
+    }
+
+}
