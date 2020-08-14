42046c117c4dac85e957aaa596e05cb7bc2ea54c
aymanMahgoub <ayman.mahgoub1@gmail.com>
2020-08-14 21:01:12 +0200

    [create] TreeShape class and handle draw tree logic

---
+++ Src/App/TreeShape.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Src\App;
+
+use InvalidArgumentException;
+
+/**
+ * Class TreeShape
+ *
+ * @package Src\App
+ */
+class TreeShape extends AbstractShape
+{
+    public function drawShape(int $height)
+    {
+        if ($height <= 0) {
+            throw new InvalidArgumentException('Height must be greater than 0');
+        }
+        $this->drawShape($height);
+        $this->drawTree($height);
+    }
+
+    private function drawTree($height)
+    {
+        $leftPointer = $rightPointer = $height;
+        while ($height > 1) {
+            for ($i = 0; $i <= $rightPointer; $i++) {
+                if ($i >= $leftPointer) {
+                    echo "X";
+                } else {
+                    echo " ";
+                }
+            }
+            echo "\n";
+            $height--;
+            $rightPointer++;
+            $leftPointer--;
+        }
+    }
+
+}
